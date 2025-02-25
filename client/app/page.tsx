a9510d878b0bc8f0a9a9d5282c696aa0a60a14b8
Nikola Mirilo <nikolamirilo@gmail.com>
2025-02-25 23:25:24 +0100

    updated page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,10 @@
 import DataView from "@/components/DataView";
-import MapView from "@/components/MapView";
-import Sidebar from "@/components/Sidebar";
 
 export const dynamic = "force-dynamic"
 export const revalidate = 60
 
 export default async function Page() {
-  const res = await fetch("http://localhost:5000/properties", {cache: "no-store"})
+  const res = await fetch("https://realestatetrackerbackend-0ngudte9.b4a.run/properties", {cache: "no-store"})
   const data = await res.json()
   if(data)
   return (
