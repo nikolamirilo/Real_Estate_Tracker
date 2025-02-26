ac9a6cee22d1fb403e0e98a35233a2735a5e4447
Nikola Mirilo <nikolamirilo@gmail.com>
2025-02-26 23:02:33 +0100

    added route on frontend

--- client/app/api/trigger/route.ts
+++ client/app/api/trigger/route.ts
@@ -1,4 +1,15 @@
-export function GET(request: Request) {
-    fetch("https://realestatetrackerbackend-0ngudte9.b4a.run/properties/refresh-data")
-    return new Response('Hello from Vercel!');
+export async function GET(request: Request) {
+    try {
+      const response = await fetch("https://realestatetrackerbackend-0ngudte9.b4a.run/properties/refresh-data");
+      const data = await response.json();
+  
+      if (data.success) {
+        return new Response('Success!', { status: 200 });
+      } else {
+        return new Response('Fail!', { status: 500 });
+      }
+    } catch (error) {
+      return new Response('Error occurred while refreshing data.', { status: 500 });
+    }
   }
+  
