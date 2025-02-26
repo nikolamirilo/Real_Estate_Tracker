81f01710e4326603ec60089bca92eb3c56897bcf
Nikola Mirilo <nikolamirilo@gmail.com>
2025-02-26 23:10:09 +0100

    changed code to just ping route

--- client/app/api/trigger/route.ts
+++ client/app/api/trigger/route.ts
@@ -1,15 +1,8 @@
 export async function GET(request: Request) {
     try {
-      const response = await fetch("https://realestatetrackerbackend-0ngudte9.b4a.run/properties/refresh-data");
-      const data = await response.json();
-  
-      if (data.success) {
-        return new Response('Success!', { status: 200 });
-      } else {
-        return new Response('Fail!', { status: 500 });
-      }
+        await fetch("https://realestatetrackerbackend-0ngudte9.b4a.run/properties/refresh-data");
+        return new Response('Ping sent!', { status: 200 });
     } catch (error) {
-      return new Response('Error occurred while refreshing data.', { status: 500 });
+        return new Response('Error occurred while pinging.', { status: 500 });
     }
-  }
-  
+}
