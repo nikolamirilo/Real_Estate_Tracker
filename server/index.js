8f7374a4a734e88bc5507f6e0be4ffd341e626e7
Nikola Mirilo <nikolamirilo@gmail.com>
2025-02-27 22:15:49 +0100

    fixed server

--- server/index.js
+++ server/index.js
@@ -28,59 +28,54 @@ app.get('/properties', async (req, res) => {
 });
 
 app.get('/properties/refresh-data', async (req, res) => {
-  try {
-      res.write(JSON.stringify({ message: "Started with scraping data and inserting into DB" }) + '\n');
-      await pool.query(createPropertiesTable);
-      const data = await fetchData();
+    try {
+        await pool.query(createPropertiesTable);
+        const data = await fetchData();
+        let messages = [];
 
-      for (let i = 0; i < data.length; i++) {
-          const checkQuery = `
-              SELECT * FROM properties 
-              WHERE link = $1 
-          `;
-          const checkValues = [
-              data[i].link,
-          ];
+        for (let i = 0; i < data.length; i++) {
+            const checkQuery = `SELECT * FROM properties WHERE link = $1`;
+            const checkValues = [data[i].link];
+            const existingRecord = await pool.query(checkQuery, checkValues);
 
-          const existingRecord = await pool.query(checkQuery, checkValues);
+            if (existingRecord.rows.length > 0) {
+                messages.push(`Property ${i + 1} already exists. Skipping.`);
+                continue;
+            }
 
-          if (existingRecord.rows.length > 0) {
-              console.log(`Property ${i + 1} already exists in the database. Skipping insertion.`);
-              continue; // Skip this iteration if the record already exists
-          }
+            const insertQuery = `
+                INSERT INTO properties (details, street, image, city_area, price, price_per_m2, is_match, link, lat, lon)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            `;
+            const insertValues = [
+                data[i].details,
+                data[i].street,
+                data[i].image,
+                data[i].cityArea,
+                data[i].price,
+                data[i].pricePerM2,
+                data[i].isMatch,
+                data[i].link,
+                data[i].lat,
+                data[i].lon,
+            ];
 
-          const insertQuery = `
-              INSERT INTO properties (details, street, image, city_area, price, price_per_m2, is_match, link, lat, lon)
-              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-          `;
-          const insertValues = [
-              data[i].details,
-              data[i].street,
-              data[i].image,
-              data[i].cityArea,
-              data[i].price,
-              data[i].pricePerM2,
-              data[i].isMatch,
-              data[i].link,
-              data[i].lat,
-              data[i].lon,
-          ];
+            try {
+                await pool.query(insertQuery, insertValues);
+                await sendMessageToDiscord(data[i]);
+                messages.push(`Property ${i + 1} inserted successfully.`);
+            } catch (err) {
+                messages.push(`Error inserting property ${i + 1}: ${err.message}`);
+            }
+        }
 
-          try {
-              await pool.query(insertQuery, insertValues);
-              await sendMessageToDiscord(data[i]);
-              console.log(`Property ${i + 1} inserted successfully.`);
-          } catch (err) {
-              console.error(`Error inserting property ${i + 1}:`, err);
-          }
-      }
-      res.status(200).json(({ message: "Finished with adding data", success: true }));
-  } catch (err) {
-      console.error('Error in /properties/refresh-data:', err);
-      res.status(500).json({ message: "Internal Server Error", error: err.message, success: false });
-  }
+        res.json({ message: "Finished adding data", details: messages, success: true });
+
+    } catch (err) {
+        console.error('Error in /properties/refresh-data:', err);
+        res.status(500).json({ message: "Internal Server Error", error: err.message, success: false });
+    }
 });
-
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
