61ed2b6092be8b6897c99822449bc591c4f98c3c
Nikola Mirilo <nikolamirilo@gmail.com>
2025-03-13 21:02:17 +0100

    fixed scraper issue

--- server/web_scrapers/4zidaScraper.js
+++ server/web_scrapers/4zidaScraper.js
@@ -6,8 +6,8 @@ import * as cheerio from 'cheerio';
 export async function scrape4zidaOffers() {
     try {
         const results = [];
-        for (let i = 1; i < 100; i++) {
-            const url = `https://www.4zida.rs/prodaja-stanova/surcin-surcin-opstina-beograd/dvoiposoban/do-150000-evra?mesto=ledine-surcin-opstina-beograd&mesto=zemun-zemun-opstina-beograd&mesto=banovo-brdo-cukarica-cukarica-opstina-beograd&mesto=vidikovac-rakovica-opstina-beograd&mesto=stari-kosutnjak-rakovica-opstina-beograd&mesto=kosutnjak-cukarica-cukarica-opstina-beograd&mesto=novi-beograd-beograd&struktura=trosoban&struktura=troiposoban&struktura=cetvorosoban&struktura=cetvoroiposoban-i-vise&vece_od=50m2&odrednica=278&stanje=u_izgradnji&stanje=novo&strana=${i}`;
+        for (let i = 1; i < 50; i++) {
+            const url = `https://www.4zida.rs/prodaja-stanova/surcin-surcin-opstina-beograd/dvoiposoban/do-180000-evra?mesto=ledine-surcin-opstina-beograd&mesto=zemun-zemun-opstina-beograd&mesto=banovo-brdo-cukarica-cukarica-opstina-beograd&mesto=vidikovac-rakovica-opstina-beograd&mesto=stari-kosutnjak-rakovica-opstina-beograd&mesto=kosutnjak-cukarica-cukarica-opstina-beograd&mesto=novi-beograd-beograd&mesto=bezanijska-kosa-novi-beograd-beograd&mesto=ledine-novi-beograd-beograd&mesto=nova-galenika-galenika-zemun-zemun-opstina-beograd&struktura=trosoban&struktura=troiposoban&struktura=cetvorosoban&struktura=cetvoroiposoban-i-vise&struktura=dvosoban&vece_od=55m2&odrednica=278&skuplje_od=100000eur&stanje=u_izgradnji&stanje=novo&strana=${i}`;
             const { data } = await axios.get(url);
             const $ = cheerio.load(data);
             $('div[class*="flex h-[180px]"]').each((i, element) => {
@@ -24,8 +24,8 @@ export async function scrape4zidaOffers() {
                 const price = titleSection.find('p:first-child').text().trim();
                 const link = $(element).find('div.relative.w-1\\/3.min-w-\\[125px\\].flex-shrink-0 a:first-child').attr('href');
                 const pricePerM2 = titleSection.find('p:last-child').text().trim();
-                const image = $('div.relative.size-full.bg-black\\/50 img').attr('srcset');
-                if (details != "" && image)
+                const image = $(element).find('img').attr("src") 
+                if (details != "")
                     results.push({
                         details: details,
                         street,
@@ -33,12 +33,14 @@ export async function scrape4zidaOffers() {
                         cityArea,
                         price: parseInt(price.replace(/[^0-9]/g, ''), 10),
                         pricePerM2: parseInt(pricePerM2.replace(/[^0-9]/g, ''), 10),
-                        image: image.includes(",") ? image.split(',').at(1) : image
+                        image: image && image.includes(",") ? image.split(',').at(1) : image
                     });
             });
         }
+        console.log(results)
         return results;
     } catch (error) {
         console.error('Error scraping data:', error);
     }
 }
+scrape4zidaOffers()
